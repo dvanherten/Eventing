76a6a6c44138c41a233a4fa5b74cd97fff2c6288
Dave van Herten <dvanherten@outlook.com>
2018-07-17 22:11:26 -0400

    Add event stream base class and repository abstraction

---
+++ OneThingWell.Eventing/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneThingWell.Eventing
+{
+    public abstract class Event
+    {
+        public Guid EventId { get; }
+        public DateTime EventDate { get; }
+
+        protected Event()
+        {
+            EventId = Guid.NewGuid();
+            EventDate = DateTime.UtcNow;
+        }
+    }
+}
---
+++ OneThingWell.Eventing/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OneThingWell.Eventing
+{
+    public interface IRepository<T> where T : EventStream
+    {
+        Task Save(EventStream eventStream, int expectedVersion);
+        Task<T> GetByStreamId(Guid id);
+    }
+}
